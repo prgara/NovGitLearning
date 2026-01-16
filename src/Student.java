71986dae5c0c17ed5641dae6ee7eaab642ec43d1
Ahmad <ahmad@example.com>
2026-01-15 21:35:58 -0500

    Add majorSubject and dob to Student class

--- src/Student.java
+++ src/Student.java
@@ -1,10 +1,30 @@
 class Student{
     String name;
     String roolNo;
-
-    public Student(String name, String roolNo) {
+String majorSubject;
+String dob;
+//h
+    public Student(String name, String roolNo, String majorSubject, String dob) {
         this.name = name;
         this.roolNo = roolNo;
+        this.majorSubject = majorSubject;
+        this.dob = dob;
+    }
+
+    public String getMajorSubject() {
+        return majorSubject;
+    }
+
+    public void setMajorSubject(String majorSubject) {
+        this.majorSubject = majorSubject;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
     }
 
     public String getName() {
