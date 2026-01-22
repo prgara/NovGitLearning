6e0b92f5e0dad10b6ec3bccef00a4a3c6ae61164
Shikha <shikhamittal23@yahoo.com>
2026-01-22 13:53:46 -0500

    comment added by shikha

--- src/Student.java
+++ src/Student.java
@@ -1,18 +1,35 @@
 class Student{
     String name;
+<<<<<<< HEAD
     String rollNumber;
 
     public Student(String name, String rollNo) {
         this.name = name;
         this.rollNumber = rollNo;
+=======
+    String roolNo;
+    String ID;
+
+
+    String majorSub;
+    String subject;
+    String dob;
+
+   public Student(String name, String roolNo, String subject, String dob) {
+        this.name = name;
+        this.roolNo = roolNo;
+        this.subject = subject;
+        this.dob = dob;
+>>>>>>> 9e187aef935de8c244059eb255910dcc02d5c1f6
     }
 
     public String getName() {
+
         return name;
     }
 // Shikha Method
     public void setName(String name) {
-        this.name = name;
+        this.name = name+" ";
     }
 
     public String getRollNo() {
@@ -21,5 +38,21 @@ class Student{
 
     public void setRollNo(String rollNo) {
         this.rollNumber = rollNo;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+//Shikha Comments
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
     }
 }
